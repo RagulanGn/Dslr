a3bcefa939a3688758008a6d19d893d1ec9b7cb4
RagulanGn <113636027+RagulanGn@users.noreply.github.com>
2026-02-19 23:27:01 +0100

    Better dataset, but precision is still a bit off

--- logreg_predict.py
+++ logreg_predict.py
@@ -13,14 +13,18 @@ def main(dataset_path, weight_path):
 	
 	dataset = pd.read_csv(dataset_path, index_col=0)
 	dataset = dataset.dropna(axis=1, how='all')
+	dataset = dataset[int(len(dataset.index) * 0.8):]
 	hogwart_house = 'Hogwarts House' in dataset
 	if (hogwart_house):
 		dataset.set_index("Hogwarts House", append=True, inplace=True) #Add Hogwart House as index
 	#Should I drop NaN rows
 	dataset = dataset.select_dtypes(include=np.number) #Drop every NaN columns and any NaN row
+	# dataset = dataset.drop(columns=["Arithmancy", "Astronomy", "Care of Magical Creatures", "Potions"])
+	dataset = dataset.drop(columns=["Arithmancy", "Muggle Studies", "History of Magic","Care of Magical Creatures", "Potions"])
+
 	dataset.fillna(mean, inplace=True)
 	dataset = (dataset - mean) / std #Standardization to avoid overflow
-	
+	dataset["biais"] = 1
 	# sigmoide (Dataset . weight) (produit scalaire) => Plus x est grand ou petit plus le resultat est sur
 	# Donc faire l'operation sur les 4 classe (4 poids) et prendre le plus grand
 	# Pas besoin de la sigmoide car on travaille sur une fonction croissante
@@ -43,3 +47,9 @@ if __name__ == "__main__":
 # Fill only test 0 (0.960625)
 # Fill both with mean (0.9625)
 # Fill train with mean and test with 0 (0.98)
+
+
+# avec potion + 3 autres (0.98) Full set
+# dataset = dataset.drop(columns=["Arithmancy", "Astronomy", "Care of Magical Creatures", "Potions"])
+
+#0.9718
--- logreg_train.py
+++ logreg_train.py
@@ -8,6 +8,14 @@ import traceback
 
 # Courbe sigmoide => Double asymptote horinzontale a y=1 et y=-1
 
+# Data selection
+# "Defense against the dark art" IS HIGHLY COROLATED "Astronomy" Can get reed of one
+# "Arithmancy" DOES NOT SEEMS TO HAVE A COROLATION WITH ANY "THE HOGWART HOUSES"
+# "Care of magical creature" -------------------
+
+# "Potion" CAN be ignore
+
+
 #If we want to start the b at 0 we need to take care of dividing by 0
 def sigmoide(x):
     x_safe = np.clip(x, -500, 500)
@@ -18,19 +26,23 @@ def main(path):
 	df2 = df1.set_index("Hogwarts House", append=True) #Add Hogwart House as index
 	# df = df2.select_dtypes(include=np.number).dropna(axis=1, how='all') #Drop every NaN columns
 	df = df2.select_dtypes(include=np.number).dropna(axis=1, how='all') #Drop every NaN columns
+	df = df.drop(columns=["Arithmancy", "Muggle Studies", "History of Magic","Care of Magical Creatures", "Potions"])
 	df = df.fillna(df.mean())
 	#Solution against NaN => drop the entire row. Should I found a better solution ?
 	mean = df.mean()
 	std = df.std()
 	df = (df - mean) / std #Standardization to avoid overflow
+	df = df[:int(len(df.index) * 0.8)]
+	df.insert(0, 'biais', 1)
 	# print(df)
-	a = 0.01
-	b1 = np.ones(13) * 0.1
-	b2 = np.ones(13) * 0.1
-	b3 = np.ones(13) * 0.1
-	b4 = np.ones(13) * 0.1
+	a = 0.1
+	df_len = len(df.columns)
+	b1 = np.ones(df_len) * 0.1
+	b2 = np.ones(df_len) * 0.1
+	b3 = np.ones(df_len) * 0.1
+	b4 = np.ones(df_len) * 0.1
 	i = 0
-	n = 13
+	n = len(df.index)
 	index = df.index.get_level_values("Hogwarts House").to_numpy()
 	while (True) :
 		b1 = b1 - (a/n) * (df.T @ (sigmoide(df @ b1) - (index == "Ravenclaw").astype(int)))
